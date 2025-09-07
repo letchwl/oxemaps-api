198fcdca4a4122a0a0cda13c7ebe68b2d6171425
letchwl <raullalvespe1227@outlook.com.br>
2025-09-07 18:20:10 -0300

    Contador de Visitas

--- src/main/java/br/com/oxemaps/mapa/model/Visita.java
+++ src/main/java/br/com/oxemaps/mapa/model/Visita.java
@@ -1,11 +1,9 @@
 package br.com.oxemaps.mapa.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "visitas")
 public class Visita {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
